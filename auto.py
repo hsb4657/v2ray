fd2aa7bfe115272ba7237296d9bc65e32cef3b83
hsb4657 <619744299@qq.com>
2023-01-28 18:54:36 +0800

    11111

---
+++ auto.py
@@ -0,0 +1,19 @@
+import yaml
+import sys
+
+if __name__ == '__main__':
+    with open(sys.argv[1], 'r', encoding="utf-8") as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+        if not data['proxy-groups'] is None and not data['proxies'] is None:
+            proxies = list(map(lambda x: x['name'], data['proxies']))
+            proxy_group = {}
+            proxy_group['name'] = '♻️ 自动选择'
+            proxy_group['type'] = 'url-test'
+            proxy_group['url'] = 'http://www.gstatic.com/generate_204'
+            proxy_group['interval'] = 300
+            proxy_group['proxies'] = proxies
+            data['proxy-groups'].append(proxy_group)
+            data['proxy-groups'][0]['proxies'].insert(1, '♻️ 自动选择')
+            with open(sys.argv[2], 'w', encoding='utf-8') as f1:
+                yaml.safe_dump(data, f1, allow_unicode=True)
+
