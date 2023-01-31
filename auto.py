e7eb8ef854f6755e976e46ce7c1d85b0d993ced5
hsb4657 <619744299@qq.com>
2023-01-31 15:32:46 +0800

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,10 +1,11 @@
 import yaml
 import sys
+import shutil
 
 if __name__ == '__main__':
     with open(sys.argv[1], 'r', encoding="utf-8") as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
-        if not data['proxy-groups'] is None and not data['proxies'] is None:
+        if not data['proxy-groups'] is None and not data['proxies'] is None and not any(proxie['type'] =='url-test' for proxie in data['proxy-groups']):
             proxies = list(map(lambda x: x['name'], data['proxies']))
             proxy_group = {}
             proxy_group['name'] = '♻️ 自动选择'
@@ -16,4 +17,6 @@ if __name__ == '__main__':
             data['proxy-groups'][0]['proxies'].insert(1, '♻️ 自动选择')
             with open(sys.argv[2], 'w', encoding='utf-8') as f1:
                 yaml.safe_dump(data, f1, allow_unicode=True)
+        else:
+            shutil.copy(sys.argv[1], sys.argv[2])
 
