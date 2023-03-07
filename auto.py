5d6f974aa75245c5d5480f8df7f31e6d1561f23f
hsb4657 <619744299@qq.com>
2023-03-07 14:38:27 +0800

    Update auto.py

--- auto.py
+++ auto.py
@@ -1,10 +1,11 @@
-import yaml
+from ruamel.yaml import YAML
 import sys
 import shutil
 
 if __name__ == '__main__':
     with open(sys.argv[1], 'r', encoding="utf-8") as f:
-        data = yaml.load(f, Loader=yaml.FullLoader)
+        yaml = YAML()
+        data = yaml.load(f)
         if not data['proxy-groups'] is None and not data['proxies'] is None and not any(proxie['type'] =='url-test' for proxie in data['proxy-groups']):
             proxies = list(map(lambda x: x['name'], data['proxies']))
             proxy_group = {}
@@ -16,7 +17,7 @@ if __name__ == '__main__':
             data['proxy-groups'].append(proxy_group)
             data['proxy-groups'][0]['proxies'].insert(1, '♻️ 自动选择')
             with open(sys.argv[2], 'w', encoding='utf-8') as f1:
-                yaml.safe_dump(data, f1, allow_unicode=True)
+                yaml.dump(data, f1)
         else:
             shutil.copy(sys.argv[1], sys.argv[2])
 
